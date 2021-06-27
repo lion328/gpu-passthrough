b1f41eb2cc6d9787e302549f3e82c61b8c3b68dd
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2021-06-27 16:23:08 +0700

    Enable TPM and other stuff

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 
 cd edk2
 . edksetup.sh BaseTools
-build -DSECURE_BOOT_ENABLE=TRUE
+
+build \
+    -D SECURE_BOOT_ENABLE \
+    -D SMM_REQUIRE \
+    -D TPM_ENABLE \
+    -D TPM_CONFIG_ENABLE \
+    -D NETWORK_TLS_ENABLE \
+    -D NETWORK_IP6_ENABLE \
+    -D NETWORK_HTTP_BOOT_ENABLE
