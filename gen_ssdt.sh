d0af458c4989d1983fd1c261810934a07840418d
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2024-02-14 13:26:01 +0700

    Update edk2 and tools

--- gen_ssdt.sh
+++ gen_ssdt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 EDK2_HOME="$PWD/edk2"
-SRC_PATH="$EDK2_HOME/OvmfPkg/AcpiPlatformDxe"
+SRC_PATH="$EDK2_HOME/OvmfPkg/Library/AcpiPlatformLib"
 SSDT_PATH="$PWD/ssdt.asl"
 
 cd "$SRC_PATH"
 
-iasl -f -p Ssdt "ssdt.asl"
+iasl -f -p Ssdt "$SSDT_PATH"
 xxd -c1 Ssdt.aml | tail -n +37 | cut -f2 -d' ' | paste -sd' ' | sed 's/ //g' | xxd -r -p > vrom_table.aml
 xxd -i vrom_table.aml | sed 's/vrom_table_aml/vrom_table/g' > vrom_table.h
